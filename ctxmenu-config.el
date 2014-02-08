de749416f3a0c306394881d9af362a026eddadc6
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2014-02-08 20:58:26 +0900

    remove Package-Requires

--- ctxmenu-config.el
+++ ctxmenu-config.el
@@ -6,7 +6,6 @@
 ;; Keywords: popup
 ;; URL: https://github.com/aki2o/emacs-ctxmenu
 ;; Version: 0.0.1
-;; Package-Requires: ((ctxmenu "0.0.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
